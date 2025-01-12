80f11ffd0baa884f98c08ce33986e044dfcb00c5
0xnehasingh <nehasingh2mau@gmail.com>
2025-01-12 23:34:29 +0530

    constants file

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+import loanABI from "./Loan.json";
+import tokenABI from "./Token.json";
+export const loanAbi = loanABI.abi;
+export const tokenAbi = tokenABI.abi;
+export const loanContractAddress = "0x5b5Cd9dF2006beC9Be2573a932165ABA3078762B";
+export const tokenContractAddress =
+    "0x2527c67b48A9a4D504a3128d8ae41ddE3cF7F770";
